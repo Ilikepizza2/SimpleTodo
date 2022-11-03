f55fbc2f7c59e03e2516857702c1aa3ef8d9d6fb
Ilikepizza2 <lit2021010@iiitl.ac.in>
2022-11-04 04:08:09 +0530

    minor testing

--- pages/demo.js
+++ pages/demo.js
@@ -24,8 +24,8 @@ export default function demo() {
     <div className="w-full h-full bg-gray-300">
         <div className="w-full h-full flex flex-col justify-center items-center p-4 transition-all">
             <h1 className="text-4xl font-bold">{data.title}</h1>
-            <h2 className="text-3xl font-bold m-10 text-green-400 hover:underline hover:text-green-600" href="../">{data.name}</h2>
-            <p className="text-2xl md:my-10 mx-4">{data.para1}</p>
+            <a className="text-3xl font-bold m-10 text-green-400 hover:underline hover:text-green-600" href="../">{data.name}</a>
+            <p className="text-xl md:my-10 mx-4">{data.para1}</p>
             <h2 className="text-4xl font-bold">Tech Stack</h2>
             <div className="w-full  flex justify-center items-center p-4">
                 <div className="w-full h-full flex flex-col justify-center items-center p-4">
