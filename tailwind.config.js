d953d3b66bd8e1ca6e3c6f6ea293258bd8ca4332
Ilikepizza2 <lit2021010@iiitl.ac.in>
2022-11-04 04:24:47 +0530

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-module.exports = {
+  module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
   },
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
